23a482493fc7e4069c84ee77570ce6f167b7ddca
AneliBem <tolya.bem@gmail.com>
2025-05-14 19:26:50 +0300

    add model category

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, qnique=True)
+    slug = models.SlugField(unique=True)
+
+
+    class Meta:
+        ordering = ['name',]
+
+        indexes = [models.Index(fields=['name'])] #?
+
+        verbose_name = 'cateory'
+        verbose_name_plural = 'categries'
+
+
+    def __str__(self):
+        return self.name
+    
+    
