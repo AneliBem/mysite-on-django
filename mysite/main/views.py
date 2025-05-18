c6324d01411de06ccf2887d036fc0fb222005d21
AneliBem <tolya.bem@gmail.com>
2025-05-18 20:31:23 +0300

    add urls and views, connect static file

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,5 +1,16 @@
-from django.views.generic import ListView, DetailView
-from .models import Category, Product
-from django.db.models import Q
+from django.shortcuts import render, get_object_or_404
+from .models import Category
+
+def categories(request, category_slug=None):
+    categories = Category.objects.all()
+
+    category = None
+    if category_slug:
+        category = get_object_or_404(Category, category_slug=category_slug)
+        
+    return render(request, 'main/category/list.html',
+                  {'category': category,
+                   'categories': categories})
+
 
 
