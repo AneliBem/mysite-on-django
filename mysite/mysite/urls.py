231e0ae3577c146fc2330610ddda8e2dcd572661
AneliBem <tolya.bem@gmail.com>
2025-05-19 19:36:19 +0300

    add static and html and fixed errors

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,3 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
