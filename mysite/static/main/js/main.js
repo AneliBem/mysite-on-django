060ebb59afed5e055f4f2d24b1bc4f73f205ea44
AneliBem <tolya.bem@gmail.com>
2025-06-15 21:53:15 +0300

    add submenu in site

--- mysite/static/main/js/main.js
+++ mysite/static/main/js/main.js
@@ -1,7 +1,19 @@
 const burger = document.getElementById('burger');
 const nav = document.getElementById('nav');
+const submenuToggle = document.getElementById('submenu-toggle');
+const submenu = document.getElementById('submenu');
 
-burger.addEventListener('click', () => {
-    burger.classList.toggle('active');
-    nav.classList.toggle('active');
-});
+if (burger && nav) {
+	burger.addEventListener('click', () => {
+	    burger.classList.toggle('active');
+	    nav.classList.toggle('active');
+	});
+}
+
+
+if (submenuToggle && submenu) {
+	submenuToggle.addEventListener('click', (e) => {
+		e.preventDefault();
+	    submenu.classList.toggle('open');
+	});
+}
